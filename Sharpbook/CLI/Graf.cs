aa6cea29ac0837dd1b45a174b7b9962e6a5d60ea
andrcyes <andrcyes@gmail.com>
2021-03-16 18:29:06 +0700

    Add graf

---
+++ Sharpbook/CLI/Graf.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI
+{
+	class Graf
+	{
+		/*--- Fields ---*/
+		List<Simpul> simpuls; // adjacency list
+		int nSimpul; // jumlah simpul
+
+		/*--- Methods ---*/
+		// default constructor
+		public Graf()
+		{
+			this.simpuls = new List<Simpul>();
+			this.nSimpul = 0;
+		}
+
+		// copy constructor
+		public Graf(Graf _graf)
+		{
+			this.simpuls = new List<Simpul>();
+			for (int i = 0; i < _graf.nSimpul; i++)
+			{
+				this.simpuls.Add(_graf.simpuls[i]);
+			}
+			this.nSimpul = _graf.nSimpul;
+		}
+
+		// menampilkan info graf
+		public void ShowGraf()
+		{
+			if (nSimpul == 0)
+			{
+				Console.WriteLine("Graf kosong");
+			}
+			else 
+			{
+				for (int i = 0; i < nSimpul; i++)
+				{
+					this.simpuls[i].ShowSimpul();
+				}
+			}
+		}
+
+		// menambahkan simpul ke dalam graf
+		public void AddSimpul(Simpul _simp)
+		{
+			this.simpuls.Add(_simp);
+			this.nSimpul++;
+		}
+
+		// menghapus simpul dari graf dan mengembalikan simpul yang dihapus
+		public Simpul DelSimpul(string _namaSimp)
+		{
+			int i = 0;
+			while (i < this.nSimpul && this.simpuls[i].GetNama() != _namaSimp) { i++; }
+
+			Simpul _simp = this.simpuls[i];
+
+			this.simpuls.RemoveAt(i);
+			nSimpul--;
+
+			return _simp;
+		}
+	}
+
+	class Simpul
+	{
+		/*--- Fields ---*/
+		string nama; // nama simpul
+		List<string> bersisian; // list simpul yang bersisian
+		int nBersisian; // jumlah simpul yang bersisian
+
+		/*--- Methods ---*/
+		// ctor dengan parameter nama and bersisian
+		public Simpul(string _nama)
+		{
+			this.nama = _nama;
+			this.bersisian = new List<string>();
+			this.nBersisian = 0;
+		}
+
+		// getter
+		public string GetNama()
+		{
+			return this.nama;
+		}
+
+		// menambahkan simpul yang bersisian
+		public void AddBersisian(string _namaB)
+		{
+			this.bersisian.Add(_namaB);
+			this.nBersisian++;
+		}
+
+		// menampilkan info simpul
+		public void ShowSimpul()
+		{
+			Console.Write(this.nama);
+			Console.Write(": ");
+			if (nBersisian != 0)
+			{
+				for (int i = 0; i < nBersisian; i++)
+				{
+					Console.Write(this.bersisian[i]);
+					if (i != nBersisian - 1)
+					{
+						Console.Write(", ");
+					}
+				}
+				Console.WriteLine();
+			}
+			else { Console.WriteLine("-"); }
+		}
+	}
+}
---
+++ Sharpbook/CLI/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Graf g = new Graf();
+			Simpul a = new Simpul("a");
+			a.AddBersisian("b");
+			a.AddBersisian("c");
+			Simpul b = new Simpul("b");
+			b.AddBersisian("a");
+			Simpul c = new Simpul("c");
+			c.AddBersisian("a");
+			g.AddSimpul(a);
+			g.AddSimpul(b);
+			g.AddSimpul(c);
+			g.ShowGraf();
+		}
+	}
+}
